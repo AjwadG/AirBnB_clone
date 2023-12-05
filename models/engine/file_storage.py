2f1d65c5a53a9e1b0ef0ad1456521a64a65e8cbe
franklinAnozie <cfanozie@gmail.com>
2023-12-05 19:00:15 +0100

    implemented file storage and fixed issues with basemodel

---
+++ models/engine/file_storage.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+''' file storage module '''
+import json
+
+
+class FileStorage(object):
+    ''' serializes and deserializes objects to
+        and from json files
+    '''
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        ''' returns the dic of the class
+        '''
+        return self.__objects
+
+    def new(self, obj):
+        ''' sets in __object the obj
+            with the key <class name>.<id>
+        '''
+        key = f"{obj.id}"
+        self.__objects.update({key: obj.to_dict()})
+
+    def save(self):
+        ''' saves the object to a json file '''
+        new_obj = {}
+        for key, value in self.__objects.items():
+            new_obj.update({key: value})
+        with open(self.__file_path, 'w') as file:
+            json.dump(new_obj, file)
+
+    def reload(self):
+        ''' reloads the object from a json file '''
+        try:
+            with open(self.__file_path, 'r') as file:
+                file_content = json.load(file)
+                for key, value in file_content.items():
+                    self.__objects.update({key: value})
+        except FileNotFoundError:
+            pass
